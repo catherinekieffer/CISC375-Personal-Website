6c617425f1357682c86b0f7e979b6d648855c5ef
Catherine Kieffer <kief4528@stthomas.edu>
2023-09-28 14:50:37 -0500

    More changes

---
+++ js/vendor/jstest.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+let x = 20;
+// this 20 should be available anywhere if we do type application/javascript
+//this 20 will only be available at the javascript level fi we do type="module"
